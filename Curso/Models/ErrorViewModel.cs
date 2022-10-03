ef3495d531f8ee4c8c677605b941d796ab7e0e17
Javier <jmartin@grupoloyal.com>
2022-10-03 11:58:50 +0200

    Creación del proyecto

---
+++ Curso/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Curso.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
